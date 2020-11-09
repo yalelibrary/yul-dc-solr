ac56d1e67cb7b2ca949814ad1d983e2af0e21f0e
dylan salay <dylansalay@gmail.com>
2020-11-09 09:17:00 -0800

    Dynatrace refactor

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM solr:8
 
+RUN apt-get update && apt install curl -y && apt clean
 COPY solr/conf /opt/config
-
-COPY --from=nhd42358.live.dynatrace.com/linux/oneagent-codemodules:all / /
-ENV LD_PRELOAD /opt/dynatrace/oneagent/agent/lib64/liboneagentproc.so
+COPY ops/boot.sh /boot.sh
+ RUN chmod 755 /boot.sh
+ 
---
+++ ops/boot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -ex
+if [ ! -z "$DYNATRACE_TOKEN" ];then
+  curl -Ls -H "Authorization: Api-Token ${DYNATRACE_TOKEN}" 'https://nhd42358.live.dynatrace.com/api/v1/deployment/installer/agent/unix/default/latest?arch=x86&flavor=default' > installer.sh
+
+  /bin/sh installer.sh --set-app-log-content-access=true --set-infra-only=true --set-host-group=DC --set-host-name=${CLUSTER_NAME}-solr 2>&1 & 
+fi
+
+su  postgres -c '/docker-entrypoint.sh postgres -c max_connections=500'
