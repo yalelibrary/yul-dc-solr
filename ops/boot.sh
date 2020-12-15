13b11a3d30c80b4ee74b9e2f2bc682b5724ec4ab
McClain Looney <mlooney@users.noreply.github.com>
2020-12-15 15:56:18 -0600

    Fix for local solr dev (#19)
    * moved core initialization into boot script, if solr_core env var is
    present
    * removed entrypoint entry

--- ops/boot.sh
+++ ops/boot.sh
@@ -4,5 +4,10 @@ if [ ! -z "$DYNATRACE_TOKEN" ];then
 
   /bin/sh /tmp/installer.sh --set-app-log-content-access=true --set-infra-only=true --set-host-group=DC --set-host-name=${CLUSTER_NAME}-solr NON_ROOT_MODE=0 2>&1 & 
 fi
+if [ ! -z "${SOLR_CORE}" ];then
+  cp -rn /opt/config/ /var/solr/data/${SOLR_CORE}/conf
+  touch /var/solr/data/${SOLR_CORE}/core.properties
+  chown -R solr: /var/solr/data
+fi
 su solr -c 'docker-entrypoint.sh solr-foreground'
 
