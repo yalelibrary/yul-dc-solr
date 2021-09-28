6dd89c85c9baa2e2d466cb8556b1f100949bbbcb
McClain Looney <m@loonsoft.com>
2021-09-28 11:17:22 -0500

    Uses SOLR_HEAP variable, defaulting to 12g, overrideable via SOLR_HEAP env variable

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM solr:8
 
 COPY solr/conf /opt/config
 COPY ops/boot.sh /boot.sh
-USER root
+USER solr
+ENTRYPOINT ["/boot.sh"]
--- ops/boot.sh
+++ ops/boot.sh
@@ -2,13 +2,14 @@
 if [ ! -z "$DYNATRACE_TOKEN" ];then
   curl -Ls -H "Authorization: Api-Token ${DYNATRACE_TOKEN}" 'https://nhd42358.live.dynatrace.com/api/v1/deployment/installer/agent/unix/default/latest?arch=x86&flavor=default' > /tmp/installer.sh
 
-  /bin/sh /tmp/installer.sh --set-app-log-content-access=true --set-infra-only=true --set-host-group=DC --set-host-name=${CLUSTER_NAME}-solr NON_ROOT_MODE=0 2>&1 &
+/bin/sh /tmp/installer.sh --set-app-log-content-access=true --set-infra-only=true --set-host-group=DC --set-host-name=${CLUSTER_NAME}-solr NON_ROOT_MODE=0 2>&1 &
 fi
 if [ ! -z "${SOLR_CORE}" ];then
   cp -rn /opt/config/ /var/solr/data/${SOLR_CORE}/conf
   touch /var/solr/data/${SOLR_CORE}/core.properties
   chown -R solr: /var/solr/data
 fi
-export SOLR_HEAP=12g
+export SOLR_HEAP="${SOLR_HEAP:=12g}"
+echo $SOLR_HEAP
 
-su solr -c 'ulimit -n 65000 && docker-entrypoint.sh solr-foreground'
+ulimit -n 65000 && docker-entrypoint.sh solr-foreground
