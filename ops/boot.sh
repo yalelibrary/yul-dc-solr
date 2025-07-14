6ba088fb398448db545f646b271615c0c28f8d0f
K8Sewell <kait@notch8.com>
2025-07-14 13:17:15 -0500

    Remove dynatrace functions

--- ops/boot.sh
+++ ops/boot.sh
@@ -1,9 +1,4 @@
 #!/bin/bash -ex
-if [ ! -z "$DYNATRACE_TOKEN" ];then
-  curl -Ls -H "Authorization: Api-Token ${DYNATRACE_TOKEN}" 'https://nhd42358.live.dynatrace.com/api/v1/deployment/installer/agent/unix/default/latest?arch=x86&flavor=default' > /tmp/installer.sh
-
-/bin/sh /tmp/installer.sh --set-app-log-content-access=true --set-infra-only=true --set-host-group=DC --set-host-name=${CLUSTER_NAME}-solr NON_ROOT_MODE=0 2>&1 &
-fi
 if [ ! -z "${SOLR_CORE}" ];then
   cp -rn /opt/config/ /var/solr/data/${SOLR_CORE}/conf
   touch /var/solr/data/${SOLR_CORE}/core.properties
